95256d6e9d97f4cd441f0e3f6893b92fda026f53
Timothée Mazzucotelli <timothee.mazzucotelli@gmail.com>
2018-12-29 01:15:23 +0100

    WIP GitHub service

--- src/taskhub/core/services/github.py
+++ src/taskhub/core/services/github.py
@@ -1,64 +1,319 @@
 import os
 from github import Github
-import sys
 
 from . import Service
 
 
 class GitHubService(Service):
+    """
+    
+    Example JSON issue:
+
+    .. code:: json
+        
+        [
+          {
+            "id": 1,
+            "node_id": "MDU6SXNzdWUx",
+            "url": "https://api.github.com/repos/octocat/Hello-World/issues/1347",
+            "repository_url": "https://api.github.com/repos/octocat/Hello-World",
+            "labels_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/labels{/name}",
+            "comments_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/comments",
+            "events_url": "https://api.github.com/repos/octocat/Hello-World/issues/1347/events",
+            "html_url": "https://github.com/octocat/Hello-World/issues/1347",
+            "number": 1347,
+            "state": "open",
+            "title": "Found a bug",
+            "body": "I'm having a problem with this.",
+            "user": {
+              "login": "octocat",
+              "id": 1,
+              "node_id": "MDQ6VXNlcjE=",
+              "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+              "gravatar_id": "",
+              "url": "https://api.github.com/users/octocat",
+              "html_url": "https://github.com/octocat",
+              "followers_url": "https://api.github.com/users/octocat/followers",
+              "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+              "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+              "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+              "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+              "organizations_url": "https://api.github.com/users/octocat/orgs",
+              "repos_url": "https://api.github.com/users/octocat/repos",
+              "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+              "received_events_url": "https://api.github.com/users/octocat/received_events",
+              "type": "User",
+              "site_admin": false
+            },
+            "labels": [
+              {
+                "id": 208045946,
+                "node_id": "MDU6TGFiZWwyMDgwNDU5NDY=",
+                "url": "https://api.github.com/repos/octocat/Hello-World/labels/bug",
+                "name": "bug",
+                "description": "Something isn't working",
+                "color": "f29513",
+                "default": true
+              }
+            ],
+            "assignee": {
+              "login": "octocat",
+              "id": 1,
+              "node_id": "MDQ6VXNlcjE=",
+              "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+              "gravatar_id": "",
+              "url": "https://api.github.com/users/octocat",
+              "html_url": "https://github.com/octocat",
+              "followers_url": "https://api.github.com/users/octocat/followers",
+              "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+              "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+              "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+              "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+              "organizations_url": "https://api.github.com/users/octocat/orgs",
+              "repos_url": "https://api.github.com/users/octocat/repos",
+              "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+              "received_events_url": "https://api.github.com/users/octocat/received_events",
+              "type": "User",
+              "site_admin": false
+            },
+            "assignees": [
+              {
+                "login": "octocat",
+                "id": 1,
+                "node_id": "MDQ6VXNlcjE=",
+                "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/octocat",
+                "html_url": "https://github.com/octocat",
+                "followers_url": "https://api.github.com/users/octocat/followers",
+                "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                "organizations_url": "https://api.github.com/users/octocat/orgs",
+                "repos_url": "https://api.github.com/users/octocat/repos",
+                "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/octocat/received_events",
+                "type": "User",
+                "site_admin": false
+              }
+            ],
+            "milestone": {
+              "url": "https://api.github.com/repos/octocat/Hello-World/milestones/1",
+              "html_url": "https://github.com/octocat/Hello-World/milestones/v1.0",
+              "labels_url": "https://api.github.com/repos/octocat/Hello-World/milestones/1/labels",
+              "id": 1002604,
+              "node_id": "MDk6TWlsZXN0b25lMTAwMjYwNA==",
+              "number": 1,
+              "state": "open",
+              "title": "v1.0",
+              "description": "Tracking milestone for version 1.0",
+              "creator": {
+                "login": "octocat",
+                "id": 1,
+                "node_id": "MDQ6VXNlcjE=",
+                "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/octocat",
+                "html_url": "https://github.com/octocat",
+                "followers_url": "https://api.github.com/users/octocat/followers",
+                "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                "organizations_url": "https://api.github.com/users/octocat/orgs",
+                "repos_url": "https://api.github.com/users/octocat/repos",
+                "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/octocat/received_events",
+                "type": "User",
+                "site_admin": false
+              },
+              "open_issues": 4,
+              "closed_issues": 8,
+              "created_at": "2011-04-10T20:09:31Z",
+              "updated_at": "2014-03-03T18:58:10Z",
+              "closed_at": "2013-02-12T13:22:01Z",
+              "due_on": "2012-10-09T23:39:01Z"
+            },
+            "locked": true,
+            "active_lock_reason": "too heated",
+            "comments": 0,
+            "pull_request": {
+              "url": "https://api.github.com/repos/octocat/Hello-World/pulls/1347",
+              "html_url": "https://github.com/octocat/Hello-World/pull/1347",
+              "diff_url": "https://github.com/octocat/Hello-World/pull/1347.diff",
+              "patch_url": "https://github.com/octocat/Hello-World/pull/1347.patch"
+            },
+            "closed_at": null,
+            "created_at": "2011-04-22T13:33:48Z",
+            "updated_at": "2011-04-22T13:33:48Z",
+            "repository": {
+              "id": 1296269,
+              "node_id": "MDEwOlJlcG9zaXRvcnkxMjk2MjY5",
+              "name": "Hello-World",
+              "full_name": "octocat/Hello-World",
+              "owner": {
+                "login": "octocat",
+                "id": 1,
+                "node_id": "MDQ6VXNlcjE=",
+                "avatar_url": "https://github.com/images/error/octocat_happy.gif",
+                "gravatar_id": "",
+                "url": "https://api.github.com/users/octocat",
+                "html_url": "https://github.com/octocat",
+                "followers_url": "https://api.github.com/users/octocat/followers",
+                "following_url": "https://api.github.com/users/octocat/following{/other_user}",
+                "gists_url": "https://api.github.com/users/octocat/gists{/gist_id}",
+                "starred_url": "https://api.github.com/users/octocat/starred{/owner}{/repo}",
+                "subscriptions_url": "https://api.github.com/users/octocat/subscriptions",
+                "organizations_url": "https://api.github.com/users/octocat/orgs",
+                "repos_url": "https://api.github.com/users/octocat/repos",
+                "events_url": "https://api.github.com/users/octocat/events{/privacy}",
+                "received_events_url": "https://api.github.com/users/octocat/received_events",
+                "type": "User",
+                "site_admin": false
+              },
+              "private": false,
+              "html_url": "https://github.com/octocat/Hello-World",
+              "description": "This your first repo!",
+              "fork": true,
+              "url": "https://api.github.com/repos/octocat/Hello-World",
+              "archive_url": "http://api.github.com/repos/octocat/Hello-World/{archive_format}{/ref}",
+              "assignees_url": "http://api.github.com/repos/octocat/Hello-World/assignees{/user}",
+              "blobs_url": "http://api.github.com/repos/octocat/Hello-World/git/blobs{/sha}",
+              "branches_url": "http://api.github.com/repos/octocat/Hello-World/branches{/branch}",
+              "collaborators_url": "http://api.github.com/repos/octocat/Hello-World/collaborators{/collaborator}",
+              "comments_url": "http://api.github.com/repos/octocat/Hello-World/comments{/number}",
+              "commits_url": "http://api.github.com/repos/octocat/Hello-World/commits{/sha}",
+              "compare_url": "http://api.github.com/repos/octocat/Hello-World/compare/{base}...{head}",
+              "contents_url": "http://api.github.com/repos/octocat/Hello-World/contents/{+path}",
+              "contributors_url": "http://api.github.com/repos/octocat/Hello-World/contributors",
+              "deployments_url": "http://api.github.com/repos/octocat/Hello-World/deployments",
+              "downloads_url": "http://api.github.com/repos/octocat/Hello-World/downloads",
+              "events_url": "http://api.github.com/repos/octocat/Hello-World/events",
+              "forks_url": "http://api.github.com/repos/octocat/Hello-World/forks",
+              "git_commits_url": "http://api.github.com/repos/octocat/Hello-World/git/commits{/sha}",
+              "git_refs_url": "http://api.github.com/repos/octocat/Hello-World/git/refs{/sha}",
+              "git_tags_url": "http://api.github.com/repos/octocat/Hello-World/git/tags{/sha}",
+              "git_url": "git:github.com/octocat/Hello-World.git",
+              "issue_comment_url": "http://api.github.com/repos/octocat/Hello-World/issues/comments{/number}",
+              "issue_events_url": "http://api.github.com/repos/octocat/Hello-World/issues/events{/number}",
+              "issues_url": "http://api.github.com/repos/octocat/Hello-World/issues{/number}",
+              "keys_url": "http://api.github.com/repos/octocat/Hello-World/keys{/key_id}",
+              "labels_url": "http://api.github.com/repos/octocat/Hello-World/labels{/name}",
+              "languages_url": "http://api.github.com/repos/octocat/Hello-World/languages",
+              "merges_url": "http://api.github.com/repos/octocat/Hello-World/merges",
+              "milestones_url": "http://api.github.com/repos/octocat/Hello-World/milestones{/number}",
+              "notifications_url": "http://api.github.com/repos/octocat/Hello-World/notifications{?since,all,participating}",
+              "pulls_url": "http://api.github.com/repos/octocat/Hello-World/pulls{/number}",
+              "releases_url": "http://api.github.com/repos/octocat/Hello-World/releases{/id}",
+              "ssh_url": "git@github.com:octocat/Hello-World.git",
+              "stargazers_url": "http://api.github.com/repos/octocat/Hello-World/stargazers",
+              "statuses_url": "http://api.github.com/repos/octocat/Hello-World/statuses/{sha}",
+              "subscribers_url": "http://api.github.com/repos/octocat/Hello-World/subscribers",
+              "subscription_url": "http://api.github.com/repos/octocat/Hello-World/subscription",
+              "tags_url": "http://api.github.com/repos/octocat/Hello-World/tags",
+              "teams_url": "http://api.github.com/repos/octocat/Hello-World/teams",
+              "trees_url": "http://api.github.com/repos/octocat/Hello-World/git/trees{/sha}",
+              "clone_url": "https://github.com/octocat/Hello-World.git",
+              "mirror_url": "git:git.example.com/octocat/Hello-World",
+              "hooks_url": "http://api.github.com/repos/octocat/Hello-World/hooks",
+              "svn_url": "https://svn.github.com/octocat/Hello-World",
+              "homepage": "https://github.com",
+              "language": null,
+              "forks_count": 9,
+              "stargazers_count": 80,
+              "watchers_count": 80,
+              "size": 108,
+              "default_branch": "master",
+              "open_issues_count": 0,
+              "topics": [
+                "octocat",
+                "atom",
+                "electron",
+                "API"
+              ],
+              "has_issues": true,
+              "has_projects": true,
+              "has_wiki": true,
+              "has_pages": false,
+              "has_downloads": true,
+              "archived": false,
+              "pushed_at": "2011-01-26T19:06:43Z",
+              "created_at": "2011-01-26T19:01:12Z",
+              "updated_at": "2011-01-26T19:14:43Z",
+              "permissions": {
+                "admin": false,
+                "push": false,
+                "pull": true
+              },
+              "allow_rebase_merge": true,
+              "allow_squash_merge": true,
+              "allow_merge_commit": true,
+              "subscribers_count": 42,
+              "network_count": 0
+            }
+          }
+        ]
+    """
+
     name = "github"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         token = os.environ.get("GITHUB_TOKEN")
         if not token:
-            print("GITHUB_TOKEN environment variable must be set.")
-            sys.exit(1)
+            raise EnvironmentError("GITHUB_TOKEN environment variable must be set.")
 
         self.client = Github(token, per_page=100)
 
     def read_tasks(self, *args, **kwargs):
         user = self.client.get_user()
+        pulled_issues = []
 
         print("Gathering user repositories information...")
         repos = list(user.get_repos())
         archived_repos = set(r.full_name for r in repos if r.archived)
+        del repos
+
         print("Gathering user issues across all repositories and organizations...")
-        user_issues = list(user.get_user_issues(filter="all", state="all"))
+        pulled_issues.extend(list(user.get_user_issues(filter="all", state="all")))
+
         print("Gathering public issues created by user...")
-        created_issues = list(self.client.search_issues(query="author:pawamoy type:all archived:false"))
+        pulled_issues.extend(list(self.client.search_issues(query="author:pawamoy type:all archived:false")))
+
         print("Gathering public issues assigned to user...")
-        assigned_issues = list(self.client.search_issues(query="assignee:pawamoy type:all archived:false"))
+        pulled_issues.extend(list(self.client.search_issues(query="assignee:pawamoy type:all archived:false")))
 
         print("Building the unique set of issues...")
         seen = set()
+        seen_add = seen.add
         issues = []
-        all_issues = user_issues + created_issues + assigned_issues
-        for issue in all_issues:
-            repository_url = issue._rawData["repository_url"]
-            repository_full_name = "/".join(repository_url.split("/")[-2:])
+        for issue in pulled_issues:
+
+            # add repository full name to raw data
+            repository_full_name = "/".join(issue._rawData["repository_url"].split("/")[-2:])
             issue._rawData["repository_full_name"] = repository_full_name
-            ref = repository_url + "#" + str(issue._rawData["number"])
+
+            ref = issue._rawData["html_url"]
+
             if ref not in seen:
-                seen.add(ref)
+                seen_add(ref)
+
+                # filter out issues from archived repositories
                 if repository_full_name not in archived_repos:
                     issues.append(issue)
 
-        del all_issues
-        del user_issues
-        del created_issues
-        del assigned_issues
+        del pulled_issues
 
         print("Transforming issues into tasks...")
-        issues = [self.to_generic_task(i) for i in issues]
-        return issues
+        tasks = [self.to_generic_task(i) for i in issues]
+
+        del issues
+
+        return tasks
 
     def to_generic_task(self, service_task):
         project = "git." + service_task._rawData["repository_full_name"].lower().replace(".", "-").replace("/", ".")
-        # labels=[l['name'] for l in issue._rawData['labels']]
-        # tags=''
-        # if labels:
-        #     tags='+' + ' +'.join(labels)
 
         entry = service_task._rawData["created_at"].replace("-", "").replace(":", "")
         end = service_task._rawData["closed_at"]
@@ -66,27 +321,10 @@ class GitHubService(Service):
             end = end.replace("-", "").replace(":", "")
 
         tw_issue = dict(
-            # depends
             description="(GH) " + service_task._rawData["title"].strip(),
-            # due
             end=end,
             entry=entry,
-            # id
-            # imask
-            # mask
-            # modified
-            # parent
-            # priority
             project=project,
-            # recur
-            # scheduled
-            # start
-            # status
-            # tags=tags,
-            # until
-            # urgency
-            # uuid
-            # wait
             githuburl=service_task._rawData["html_url"],
         )
 
